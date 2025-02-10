38a02ffe7b275cb15776396e46ffc84d33f52fae
BenjaminDoran <20494558+BenjaminDoran@users.noreply.github.com>
2025-02-09 19:00:15 -0600

    fix formatting and docs env

--- docs/src/changelog.md
+++ docs/src/changelog.md
@@ -1,4 +1,5 @@
 [unreleased]
+
 - applied bestietemplates v0.14.1 for best practices
 - added basic treeplot recipe for Makie backend
 - added initial documentation
--- docs/src/literate-tutorials/basics.jl
+++ docs/src/literate-tutorials/basics.jl
@@ -6,7 +6,7 @@ basic setup
 
 using CairoMakie, TreePlots
 using AbstractTrees
-update_theme!(Theme(size=(500,500)))
+update_theme!(Theme(size = (500, 500)))
 
 # # plot the tree
 tree = ((:a, :b), (:c, (:d, :e)))
@@ -19,7 +19,7 @@ We might want to just look at the tree rather than the axis
 ==================#
 
 fig = Figure()
-ax = Axis(fig[1,1])
+ax = Axis(fig[1, 1])
 hidedecorations!(ax)
 hidespines!(ax)
 treeplot!(tree)
@@ -28,12 +28,10 @@ fig
 # Rather than the square branches we can use straight branches
 
 fig = Figure()
-ax = Axis(fig[1,1])
+ax = Axis(fig[1, 1])
 hidedecorations!(ax)
 hidespines!(ax)
-treeplot!(tree;
-    branchstyle=:straight,
-)
+treeplot!(tree; branchstyle = :straight)
 fig
 
 #=
@@ -41,7 +39,7 @@ We can plot onto a Polar axis for a circular layout
 =#
 
 fig = Figure()
-ax = PolarAxis(fig[1,1])
+ax = PolarAxis(fig[1, 1])
 hidedecorations!(ax)
 hidespines!(ax)
 treeplot!(tree)
@@ -50,28 +48,19 @@ fig
 # We can increase the tip label fontsize.
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.2),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.2))
 hidedecorations!(ax)
 hidespines!(ax)
-treeplot!(tree;
-    tipfontsize=30,
-)
+treeplot!(tree; tipfontsize = 30)
 fig
 
 # We can change the line color
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.1),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.1))
 hidedecorations!(ax)
 hidespines!(ax)
-treeplot!(tree;
-    linecolor=:orange,
-    tipfontsize=12,
-)
+treeplot!(tree; linecolor = :orange, tipfontsize = 12)
 fig
 
 # We can change the line color based on info in the tree
@@ -81,22 +70,16 @@ branchcolors = map(PreOrderDFS(tree)) do node
 end
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.1),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.1))
 hidedecorations!(ax)
 hidespines!(ax)
-treeplot!(tree;
-    linecolor=branchcolors,
-    tipfontsize=12,
-)
+treeplot!(tree; linecolor = branchcolors, tipfontsize = 12)
 fig
 
 # For instance if we have external data about each node in the tree
 
 tree_data = Dict(
-    node => (; support=rand(), favorite_number=rand(1:5))
-        for node in PreOrderDFS(tree)
+    node => (; support = rand(), favorite_number = rand(1:5)) for node in PreOrderDFS(tree)
 )
 
 # then we can plot support as the color and the favorite number as the line width.
@@ -110,50 +93,41 @@ branchwidths = map(PreOrderDFS(tree)) do node
 end
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.1),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.1))
 hidedecorations!(ax)
 hidespines!(ax)
-p = treeplot!(tree;
-    linecolor=branchcolors,
-    linewidth=branchwidths,
-    tipfontsize=12,
-)
-Colorbar(fig[1,2][3,1], p)
+p = treeplot!(tree; linecolor = branchcolors, linewidth = branchwidths, tipfontsize = 12)
+Colorbar(fig[1, 2][3, 1], p)
 fig
 
 # if we have too many leaf tips to read their labels anyway we can turn off the label visibility
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.1),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.1))
 hidedecorations!(ax)
 hidespines!(ax)
-p = treeplot!(tree;
-    linecolor=branchcolors,
-    linewidth=branchwidths,
-    tipfontsize=12,
-    tipannotationsvisible=false,
+p = treeplot!(
+    tree;
+    linecolor = branchcolors,
+    linewidth = branchwidths,
+    tipfontsize = 12,
+    tipannotationsvisible = false,
 )
-Colorbar(fig[1,2][3,1], p)
+Colorbar(fig[1, 2][3, 1], p)
 fig
 
 # For a PolarAxis, We can also control the span across which the tree is plotted. with the `openangle` parameter
 
 fig = Figure()
-ax = PolarAxis(fig[1,1],
-    rautolimitmargin=(0.0, 0.1),
-)
+ax = PolarAxis(fig[1, 1], rautolimitmargin = (0.0, 0.1))
 hidedecorations!(ax)
 hidespines!(ax)
-p = treeplot!(tree;
-    linecolor=branchcolors,
-    linewidth=branchwidths,
-    tipfontsize=12,
-    openangle=deg2rad(140)
+p = treeplot!(
+    tree;
+    linecolor = branchcolors,
+    linewidth = branchwidths,
+    tipfontsize = 12,
+    openangle = deg2rad(140),
 )
-Colorbar(fig[1,2][3,1], p)
+Colorbar(fig[1, 2][3, 1], p)
 fig
-
